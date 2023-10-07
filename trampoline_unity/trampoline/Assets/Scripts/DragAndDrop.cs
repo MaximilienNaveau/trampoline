161171e28924aace23fbf69372ff94b411f6de1c
Maximilien Naveau <maximilien.naveau@gmail.com>
2023-10-07 23:32:25 +0200

    Tune the grid and create aworkable drag and drop scripts working with the tiles.

--- trampoline_unity/trampoline/Assets/Scripts/DragAndDrop.cs
+++ trampoline_unity/trampoline/Assets/Scripts/DragAndDrop.cs
@@ -5,33 +5,51 @@ using UnityEngine.EventSystems;
 
 public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
+    private Vector2 startDragPosition_;
+    private bool draggedOnTile_;
     private RectTransform rectTransform_;
+    private CanvasGroup canvasGroup_;
+
+    private Canvas canvas_;
 
     private void Awake()
     {
         rectTransform_ = GetComponent<RectTransform>();
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("DragAndDrop::OnBeginDrag(): Test");
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        Debug.Log("DragAndDrop::OnDrag(): Test");
-        rectTransform_.anchoredPosition += eventData.delta;
+        canvasGroup_ = GetComponent<CanvasGroup>();
+        canvas_ = GameObject.FindGameObjectWithTag(
+            "StaticCanvas").GetComponent<Canvas>();
+        draggedOnTile_ = false;
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("DragAndDrop::OnEndDrag(): Test");
+        canvasGroup_.alpha = 0.6f;
+        canvasGroup_.blocksRaycasts = false;
+        startDragPosition_ = rectTransform_.anchoredPosition;
+        draggedOnTile_ = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform_.anchoredPosition += eventData.delta / canvas_.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup_.alpha = 1.0f;
+        canvasGroup_.blocksRaycasts = true;
+        if(!draggedOnTile_)
+        {
+            rectTransform_.anchoredPosition = startDragPosition_;
+        }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("DragAndDrop::OnPointerDown(): Test");
     }
 
-    
+    public void setDraggedOnTile(bool draggedOnTile)
+    {
+        draggedOnTile_ = draggedOnTile;
+    }
 }
--- trampoline_unity/trampoline/Assets/Scripts/Tile.cs
+++ trampoline_unity/trampoline/Assets/Scripts/Tile.cs
@@ -1,21 +1,54 @@
 // using System.Collections;
 // using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Tile : MonoBehaviour
+public class Tile : MonoBehaviour, IDropHandler
 {
     // Attributes
-    
+    private RectTransform rectTransform_;
+    private RectTransform rowRectTransform_;
+    private RectTransform boardRectTransform_;
+    private Vector2 rowTopLeftCorner_;
+    private Vector2 boardTopLeftCorner_;
+    private Vector2 absolutePosition_;
+
+    private void Awake()
+    {
+        rowRectTransform_ = transform.parent.gameObject.GetComponent<RectTransform>();
+        boardRectTransform_ = transform.parent.parent.gameObject.GetComponent<RectTransform>();
+        rectTransform_ = GetComponent<RectTransform>();
+    }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+    }
+
+    public void updateAbsolutePosition()
+    {
+        boardTopLeftCorner_.x = boardRectTransform_.anchoredPosition.x - 0.5f * boardRectTransform_.sizeDelta.x;
+        boardTopLeftCorner_.y = boardRectTransform_.anchoredPosition.y + 0.5f * boardRectTransform_.sizeDelta.y;
+        rowTopLeftCorner_.x = boardTopLeftCorner_.x + rowRectTransform_.anchoredPosition.x - 0.5f * rowRectTransform_.sizeDelta.x;
+        rowTopLeftCorner_.y = boardTopLeftCorner_.y + rowRectTransform_.anchoredPosition.y + 0.5f * rowRectTransform_.sizeDelta.y;
+        absolutePosition_ = rowTopLeftCorner_ + rectTransform_.anchoredPosition;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Dropped on a Tile");
+        if (eventData.pointerDrag != null)
+        {   
+            updateAbsolutePosition();
+            eventData.pointerDrag.GetComponent<RectTransform>().
+                anchoredPosition = absolutePosition_;
+            eventData.pointerDrag.GetComponent<DragAndDrop>().setDraggedOnTile(true);
+        }
     }
 }
