73f0787734e0600fc66aef12066f9336e83d325d
Maximilien Naveau <maximilien.naveau@gmail.com>
2024-03-31 19:12:42 +0200

    Try to create scenes

--- trampoline_unity/trampoline/Assets/Scripts/StaticCanvas.cs
+++ trampoline_unity/trampoline/Assets/Scripts/StaticCanvas.cs
@@ -5,17 +5,12 @@ using UnityEngine.UI;
 
 public class StaticCanvas : MonoBehaviour
 {
-    void Awake()
-    {
-        RectTransform trans = GetComponent<RectTransform>();
-        trans.anchoredPosition = new Vector2(
-            transform.parent.GetComponent<RectTransform>().sizeDelta.x / 2.0f - trans.sizeDelta.x / 2.0f,
-            0.0f);
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        
+      RectTransform trans = GetComponent<RectTransform>();
+        trans.anchoredPosition = new Vector2(
+            transform.parent.GetComponent<RectTransform>().sizeDelta.x / 2.0f - trans.sizeDelta.x / 2.0f,
+            0.0f);   
     }
 }
