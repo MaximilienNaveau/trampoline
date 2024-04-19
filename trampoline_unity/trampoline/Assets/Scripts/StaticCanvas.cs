20548ef4732fd68bfd32f7abf2d9e40ce2d98fcf
Maximilien Naveau <maximilien.naveau@gmail.com>
2024-04-19 23:16:06 +0200

    fix the Scene placement

--- trampoline_unity/trampoline/Assets/Scripts/StaticCanvas.cs
+++ trampoline_unity/trampoline/Assets/Scripts/StaticCanvas.cs
@@ -8,9 +8,8 @@ public class StaticCanvas : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      RectTransform trans = GetComponent<RectTransform>();
-        trans.anchoredPosition = new Vector2(
-            transform.parent.GetComponent<RectTransform>().sizeDelta.x / 2.0f - trans.sizeDelta.x / 2.0f,
-            0.0f);   
+        // GetComponent<RectTransform>().anchoredPosition = new Vector2(
+        //     transform.parent.GetComponent<RectTransform>().sizeDelta.x / 2.0f - trans.sizeDelta.x / 2.0f,
+        //     0.0f);   
     }
 }
