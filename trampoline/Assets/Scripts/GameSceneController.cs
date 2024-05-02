42dfce162807af8f76cb6ccd83b7aee9941da015
Maximilien Naveau <maximilien.naveau@gmail.com>
2024-05-02 08:23:36 +0200

    Start adding the rules and the start menu

--- trampoline/Assets/Scripts/GameSceneController.cs
+++ trampoline/Assets/Scripts/GameSceneController.cs
@@ -1,14 +1,23 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameSceneController : MonoBehaviour
+public class GameSceneController: MonoBehaviour
 {
-    // private void OnEnable()
-    // {
-    //     SceneManager.LoadScene("main_scene");
-    // }
+    public enum GameScene {
+        introduction_scene,
+        rules_scenes,
+        solo_game_scene,
+        multiplayer_scene,
+    }
 
-    public void QuitGame()
+    public static void Load(GameScene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+
+    public static void QuitGame()
     {
         Application.Quit();
         Debug.Log("Quit!");
