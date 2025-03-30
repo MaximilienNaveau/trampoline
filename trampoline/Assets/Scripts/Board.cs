4e541ceb23964c1b0878fec50f8a9227448c3b03
Maximilien Naveau <maximilien.naveau@gmail.com>
2025-03-30 18:56:48 +0200

    test grid layout

--- trampoline/Assets/Scripts/Board.cs
+++ trampoline/Assets/Scripts/Board.cs
@@ -4,17 +4,20 @@ using UnityEngine.Assertions;
 using System;
 using System.Collections.Generic;
 
+
 public class Board : MonoBehaviour
 {
-    private Row[] rows_;
+    private Tile[] tiles_;
+    private int rows_ = 9;
+    private int cols_ = 13;
     List<Word> listOfWords_ = new List<Word>();
 
     private void Awake()
     {
         listOfWords_.Clear();
-        rows_ = GetComponentsInChildren<Row>();
-        Assert.AreEqual(rows_.Length, 13);
-        for (int i = 0 ; i < rows_.Length ; i++)
+        tiles_ = GetComponentsInChildren<Tile>();
+        Assert.AreEqual(tiles_.Length, cols_ * rows_);
+        for (int i = 0 ; i < rows_ ; i++)
         {
             listOfWords_.Add(new Word());
         }
@@ -23,9 +26,23 @@ public class Board : MonoBehaviour
     public List<Word> GetListOfWords()
     {
         // Update the list of valid words and update score.
-        for (int i = 0; i < rows_.Length; i++)
+        for (int row = 0; row < rows_; row++)
         {
-            listOfWords_[i] = rows_[i].ExtractCurrentWord();
+            listOfWords_[row].word_ = "";
+            listOfWords_[row].nb_green_letters_ = 0;
+            for (int col = 0; col < cols_; col++)
+            {
+                int i = row * cols_ + col;
+                if(!tiles_[i].HasToken())
+                {
+                    break;
+                }
+                listOfWords_[row].word_ += tiles_[i].GetToken().GetLetter();
+                if(tiles_[i].GetToken().IsOnGreenFace())
+                {
+                    listOfWords_[row].nb_green_letters_++;
+                }
+            }
         }
         return listOfWords_;
     }
